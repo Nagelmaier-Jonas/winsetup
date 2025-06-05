6ed0edc29e3475be0ceed382ab67807667883ce0
Jonas Nagelmaier <joe.nagelmaier@gmail.com>
2025-06-05 22:11:27 +0200

    fixed background task

--- winsetup.bat
+++ winsetup.bat
@@ -13,10 +13,19 @@ set "EXTENSIONS=png jpg jpeg bmp"
 set "IMG_PATH="
 
 for %%E in (%EXTENSIONS%) do (
+    echo Search: %%E
     curl -s -L "!IMG_URL_BASE!.%%E" -o "%IMG_DIR%\background.%%E"
+
     if exist "%IMG_DIR%\background.%%E" (
-        set "IMG_PATH=%IMG_DIR%\background.%%E"
-        goto :found
+        for %%I in ("%IMG_DIR%\background.%%E") do set "FILESIZE=%%~zI"
+        if !FILESIZE! gtr 1000 (
+            set "IMG_PATH=%IMG_DIR%\background.%%E"
+            echo Found and downloaded: !IMG_PATH!
+            goto :found
+        ) else (
+            echo Not found: background.%%E
+            del /f /q "%IMG_DIR%\background.%%E" >nul 2>&1
+        )
     )
 )
 
@@ -24,11 +33,13 @@ echo Failed to download background image with known extensions.
 exit /b 1
 
 :found
-:: Set the wallpaper registry keys
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%IMG_PATH%" /f
-reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 10 /f
-reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+echo Set Wallpaper to: %IMG_PATH%
+powershell -Command "$imagePath = '%IMG_PATH%'; Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class Wallpaper { [DllImport(\"user32.dll\", CharSet=CharSet.Auto)] public static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; } '; [Wallpaper]::SystemParametersInfo(20,0,$imagePath,3) | Out-Null"
+
+:: Desktop neu laden - so funktioniert es meist sicherer:
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True >nul 2>&1
+
+echo Wallpaper set.
 
 :: ========== Install Apps ==========
 
